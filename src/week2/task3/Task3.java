ad74db61e3e76f7a21da49633abe727d4b31a79f
thangem1308 <nguyenvanthangem13081999@gmail.com>
2018-09-26 01:17:13 +0700

    Week2-Task3

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -1,6 +1,25 @@
 package week2.task3;
 
-public class Task3 {
+public class theCat {
+    // Đối tượng con mèo
+    private String name;
+    private int age;
+    private String sex;
+    public String getName(){return name;}     public void setName(String name){this.name=name;}
+    public int getAge(){return age;}     public void setName(int age){this.age=age;}
+    public String getSex(){return sex;}     public void setSex(String sex){this.sex=sex;}
+    public void food(String nameFood){
+        if (nameFood=="fish") System.out.println("The cat will eat your food!");
+        esle System.out.println("Do you kill my cat with your food (-_-) ");
+    }
+    public void meow(){
+        // TODO: Tiếng mèo kêu
+        System.out.printf("Meow");
+    }
+    public void sleep(){
+        //TODO: con mèo đã ngủ
+        System.out.println("Z...Z..z.z.zzzz");
+    }
 }
 
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
