b2e71fffc54f5c2a99a7baa852396b89fe124937
thangem1308 <nguyenvanthangem13081999@gmail.com>
2018-09-26 13:20:22 +0700

    Week2-Task3

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -1,6 +1,6 @@
 package week2.task3;
 
-public class theCat {
+class theCat {
     // Đối tượng con mèo
     private String name;
     private int age;
@@ -9,8 +9,8 @@ public class theCat {
     public int getAge(){return age;}     public void setName(int age){this.age=age;}
     public String getSex(){return sex;}     public void setSex(String sex){this.sex=sex;}
     public void food(String nameFood){
-        if (nameFood=="fish") System.out.println("The cat will eat your food!");
-        esle System.out.println("Do you kill my cat with your food (-_-) ");
+        if (nameFood=="fish") {System.out.println("The cat will eat your food!");}
+        else  {System.out.println("Do you kill my cat with your food (-_-) ");}
     }
     public void meow(){
         // TODO: Tiếng mèo kêu
@@ -21,5 +21,26 @@ public class theCat {
         System.out.println("Z...Z..z.z.zzzz");
     }
 }
-
+class theDog{
+    private String name;
+    private int age;
+    private String sex;
+    public String getName(){return name;}     public void setName(String name){this.name=name;}
+    public int getAge(){return age;}     public void setName(int age){this.age=age;}
+    public String getSex(){return sex;}     public void setSex(String sex){this.sex=sex;}
+    public void an{System.out.printf("Dang an");}
+    public void ngu{System.out.printf("Dang ngu");}
+    public void choi{System.out.printf("Dang choi");}
+}
+class theMouse{
+    private String name;
+    private int age;
+    private String sex;
+    public String getName(){return name;}     public void setName(String name){this.name=name;}
+    public int getAge(){return age;}     public void setName(int age){this.age=age;}
+    public String getSex(){return sex;}     public void setSex(String sex){this.sex=sex;}
+    public void an{System.out.printf("Dang an");}
+    public void ngu{System.out.printf("Dang ngu");}
+    public void choi{System.out.printf("Dang choi");}
+}
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
