8497e4041735c2d92f2e5659aac357a6dc761766
thangem1308 <nguyenvanthangem13081999@gmail.com>
2018-11-07 14:12:05 +0700

    week9

--- src/week9/Utils.java
+++ src/week9/Utils.java
@@ -1,4 +1,58 @@
 package week9;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Utils {
+    public static String readContentFromFile(String path) throws IOException {
+        String data = new String() ;
+        String line;
+        FileInputStream file = new FileInputStream(path);
+        InputStreamReader reader = new InputStreamReader(file);
+        BufferedReader bfreader = new BufferedReader(reader);
+        while ((line =bfreader.readLine())!=null){
+            data=data+line+"\n";
+        }
+        return data;
+    }
+
+    public static void writeContentToFile1(String path) throws IOException{
+        Scanner ip = new Scanner(System.in);
+        String data = ip.nextLine();
+        FileOutputStream file = new FileOutputStream(path);
+        OutputStreamWriter write = new OutputStreamWriter(file);
+        BufferedWriter bfwrite = new BufferedWriter(write);
+        bfwrite.write(data);
+        bfwrite.newLine();
+    }
+
+    public static void writeContentToFile2(String path) throws IOException{
+        Scanner ip = new Scanner(System.in);
+        String data = ip.nextLine();
+        FileOutputStream file = new FileOutputStream(path);
+        OutputStreamWriter write = new OutputStreamWriter(file);
+        BufferedWriter bfwrite = new BufferedWriter(write);
+        bfwrite.newLine();
+        bfwrite.write(data);
+    }
+
+    public static File findFileByName(String folderPath, String fileName){
+        String path = folderPath+"\\"+fileName;
+        File file = new File(path);
+        if (file.exists()==true){
+            System.out.println("ok");
+            return file;
+        }
+        else{
+            System.out.println("nok");
+            return null;
+        }
+    }
+
+    public static void main (String[] args) throws IOException {
+        File file = findFileByName("C:\\Users\\Nguyễn Thắng Em","readFile.txt");
+        //writeContentToFile("C:\Users\Nguyễn Thắng Em\\writeFile.txt");
+        String data = readContentFromFile("C:\\Users\\Nguyễn Thắng Em\\readFile.txt");
+        System.out.println(data);
+    }
 }
